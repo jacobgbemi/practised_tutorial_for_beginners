d4b853b5060c47f4f506cfd8c494dc3a080cfa4c
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2023-03-15 14:43:01 +0100

    update and delete

---
+++ sql_basics/update_delete.sql
@@ -0,0 +1,5 @@
+UPDATE Employee SET Fistname = 'Jag' WHERE EmployeeID = 1002;  
+
+UPDATE Employee SET Gender = 'Male', Age = 31 WHERE EmployeeID = 1002;
+
+DELETE FROM Employee WHERE EmployeeID = 1002; 
